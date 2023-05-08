03e2ce8f378d04ed0157d6b16efe0c84ea244ee7
Eleonora Mattera <m.eleonora98@live.it>
2023-05-08 10:28:30 +0200

    Update Main.java

--- ProgettoSettimanale1-PlayerMultimediale/src/playerMultimediale/Main.java
+++ ProgettoSettimanale1-PlayerMultimediale/src/playerMultimediale/Main.java
@@ -15,10 +15,6 @@ public class Main {
  		//i.show();
  		
  		ElementoMultimediale[] m =new ElementoMultimediale[5];
-// 		system.out.println(m);
-// 		for(int i =0; i<m.length; i++) {
-// 			system.out.println(m[i]);
-// 		}
  		inserisci(m);
  	
  		
@@ -29,28 +25,26 @@ public class Main {
 		Scanner s = new Scanner(System.in);
 		ElementoMultimediale m1 []= new ElementoMultimediale[5];
 		do {
-	for(int i=0; i<=5; i++) {
+	for(int i=0; i<=m1.length; i++) {
 		System.out.println("AGGIUNGI FILE:");
 		System.out.println("Inserisci che tipo di elemento visualizzare: \n 1- immagine, 2-video, 3-audio");
  		int nTipologia =s.nextInt();
  		s.nextLine();
+ 		System.out.println("inserici titolo");
+ 		String titolo = s.nextLine();
  		switch(nTipologia) {
  		case 1 :
  			System.out.println("hai selezionato IMMAGINE");
- 			System.out.println("inserici titolo");
- 			String titoloImg = s.nextLine();
  			System.out.println("iserisci luminosità");
  		    int luminositaImg = s.nextInt();
  		    s.nextLine();
- 		    Immagine im = new Immagine (titoloImg,luminositaImg);
+ 		    Immagine im = new Immagine (titolo,luminositaImg);
  		    System.out.println("Hai aggiunto: \n " + "NOME IMMAGINE: " + im.getTitle() + " LUMINOSITA:' " + im.getLuminosità());
  		    m1[i] = im; //inserisco immagine nell'array  
  		    break;
  		    
  		case 2:
  			System.out.println("hai selezionato VIDEO");
- 			System.out.println("inserisci titolo");
- 			String titoloVideo = s.nextLine();
  			System.out.println("inserisci volume");
  			int volumeVideo = s.nextInt();
  			s.nextLine();
@@ -60,35 +54,35 @@ public class Main {
  			System.out.println("inserisci luminosità");
  			int luminVideo = s.nextInt();
  			s.nextLine();
- 			Video vid = new Video(titoloVideo, volumeVideo, durataVideo, luminVideo);
+ 			Video vid = new Video(titolo, volumeVideo, durataVideo, luminVideo);
  			System.out.println("hai aggiunto: \n " + "TITOLO VIDEO: " + vid.getTitle() + " VOLUME: " + vid.getVolume() + " DURATA: " + vid.getDurata() + " LUMINOSITA' " + vid.getLuminosità());
  			m1[i]= vid;
  			break;
  			
  		case 3:
  			System.out.println("hai selezionato AUDIO");
- 			System.out.println("inserisci titolo");
- 			String titoloAudio = s.nextLine();
  			System.out.println("inserisci volume");
  			int volumeAudio = s.nextInt();
  			s.nextLine();
  			System.out.println("inserisci durata");
  			double durataAudio = s.nextDouble();
  			s.nextLine();
- 			Audio aud = new Audio(titoloAudio, volumeAudio, durataAudio);
+ 			Audio aud = new Audio(titolo, volumeAudio, durataAudio);
  			System.out.println("hai aggiunto: \n " + "TITOLO AUDIO: " + aud.getTitle() + " VOLUME: " + aud.getVolume() + " DURATA: " + aud.getDurata());
             m1[i]= aud;
             break;
  	        
  		default:
  			System.out.println("tipo non valido. Riprova");
+ 			i--;
  		}//fine switch
  		
  		for(int j =0; j<m1.length; j++) {
  			System.out.println("ELEMENTI INSERITI: " + m1[j]);
  		}
     } 
-		}while (m1.length >5);
+		}while (m1.length <5);
+	
 	
 		
 	int elemento = 1;
@@ -113,11 +107,11 @@ public class Main {
 				vid.play();
 				
 			}else{
-				System.out.println("l'elemento che cerchi non e corretto");
+				System.out.println("l'elemento che cerchi non è corretto");
 			}
 		
 		}else if(elemento == 0){
-			System.out.println("perche sei uscito vitaah? :(");
+			System.out.println(" sei uscito ");
 			s.close();
 		}
 		
