5b8282f33b89f2f8dc2f97e2a66520cf1becb827
Eleonora Mattera <m.eleonora98@live.it>
2023-05-11 15:25:32 +0200

    Esercizi giorno 7 Collections

--- EserciziGiorno7(Collection)/src/eserciziCollections/Esercizio3.java
+++ EserciziGiorno7(Collection)/src/eserciziCollections/Esercizio3.java
@@ -1,37 +1,77 @@
 package eserciziCollections;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Esercizio3 {
 	private static Scanner s = new Scanner(System.in);
+	
+static	Map<String, String> rubrica = new HashMap<String, String>();
 
 	public static void main(String[] args) {
-		
-Inserimento();
-cancella(Inserimento());
-	}
-
-	public static HashMap<String, String> Inserimento () {
-		Map<String, String> nomiContatti = new HashMap<String, String>();
-		for (int i =0; i<5; i++) {
+		boolean b = true;
+		do {
 		System.out.println("inserisci nome e numero");
 		String nome = s.nextLine();
 		String numero = s.nextLine();
-		nomiContatti.put(nome, numero);
-		
+		if (nome.equals("chiudi")) {
+			nome= "";
+			numero ="";
+			b = false;
+			System.out.println("finito");
 		}
-	System.out.println(nomiContatti);
-	return (HashMap<String, String>) nomiContatti;	
+		try {
+			inserimento(nome, numero);
+		} catch (Exception e) {
+			System.out.println("contatto già esistente");
+			e.printStackTrace();
+		}
+		}while(b);
+	}
+
+	public static  void inserimento (String nome, String numero) throws Exception {
+		if(rubrica.containsKey(nome)) {
+			throw new Exception();
+		}else {
+		rubrica.put(nome, numero);
+		}
+		
+	System.out.println(rubrica);
+
 	}
 	
-	public static void cancella(HashMap nomiContatti) {
-		nomiContatti.containsKey("ele");
-		nomiContatti.remove("ele");
+	public static void cancella(String nome) {
+		rubrica.containsKey(nome);
+		rubrica.remove(nome);
+		
+	}
+	//accedere alla chiave tramite il valore
+	public static String ricercaPersonaTramiteNumero(String numero) {
+		Set <String> keys =rubrica.keySet();
+		for(String nome : keys) {
+			if(rubrica.get(nome).equals(numero)) {
+				return nome;
+			}
+		}
+		return null;
+	}
+	 //accedere al valore tramite la chiave
+	public static String ricercaNumero(String nome) {
+		return rubrica.get(nome);
 		
 	}
 	
+	//per iterare la mappa devo conoscere la chiave
+	public static void stampa() {
+		rubrica.keySet();
+		Set <String> keys =rubrica.keySet();
+	for (String nome : keys) {
+		System.out.println("nome contatto: " + nome + " numero: " + rubrica.get(nome));
+	}
+		
 	
-	
+	}
 }
