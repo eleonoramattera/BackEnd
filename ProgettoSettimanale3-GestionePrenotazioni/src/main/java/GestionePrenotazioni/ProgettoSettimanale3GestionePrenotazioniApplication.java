c950f652a795aff2e50a51780e57aace30a32f87
Eleonora Mattera <m.eleonora98@live.it>
2023-06-05 08:50:18 +0200

    ProgettoSettimanale3-GestionePrenotazioni

---
+++ ProgettoSettimanale3-GestionePrenotazioni/src/main/java/GestionePrenotazioni/ProgettoSettimanale3GestionePrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package GestionePrenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanale3GestionePrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanale3GestionePrenotazioniApplication.class, args);
+	}
+
+}
---
+++ ProgettoSettimanale3-GestionePrenotazioni/src/test/java/GestionePrenotazioni/ProgettoSettimanale3GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanale3GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
