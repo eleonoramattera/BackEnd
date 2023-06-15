f6067e621192a6f872154cda7f50143f8d9a347a
Eleonora Mattera <m.eleonora98@live.it>
2023-06-15 17:23:49 +0200

    ProgettoSettimanale5-GestioneDispositivi

---
+++ ProgettoSettimanale5-GestioneDispositivi/src/main/java/GestioneDispositivi/ProgettoSettimanale5GestioneDispositiviApplication.java
@@ -0,0 +1,13 @@
+package GestioneDispositivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanale5GestioneDispositiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanale5GestioneDispositiviApplication.class, args);
+	}
+
+}
---
+++ ProgettoSettimanale5-GestioneDispositivi/src/test/java/GestioneDispositivi/ProgettoSettimanale5GestioneDispositiviApplicationTests.java
@@ -0,0 +1,13 @@
+package GestioneDispositivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanale5GestioneDispositiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
