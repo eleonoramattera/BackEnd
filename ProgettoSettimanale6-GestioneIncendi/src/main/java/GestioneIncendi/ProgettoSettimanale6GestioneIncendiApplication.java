8273aff6319b428274827da382e15a1658d72344
Eleonora Mattera <m.eleonora98@live.it>
2023-06-19 02:19:40 +0200

    ProgettoSettimanale5-GestioneDispositivi

---
+++ ProgettoSettimanale6-GestioneIncendi/src/main/java/GestioneIncendi/ProgettoSettimanale6GestioneIncendiApplication.java
@@ -0,0 +1,13 @@
+package GestioneIncendi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanale6GestioneIncendiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanale6GestioneIncendiApplication.class, args);
+	}
+
+}
---
+++ ProgettoSettimanale6-GestioneIncendi/src/test/java/GestioneIncendi/ProgettoSettimanale6GestioneIncendiApplicationTests.java
@@ -0,0 +1,13 @@
+package GestioneIncendi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanale6GestioneIncendiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
